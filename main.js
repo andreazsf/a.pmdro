ca8ecfe6f4f15ff453f7c386816a21130f567fda
Andrea San Felipe <andrea.sanfelipe@gmail.com>
2023-07-28 03:40:45 +0800

    UpdatedPomodoro_WithErrors
    Error1: Play button not resetting when switching modes.
    Error2: Sounds not playing
    Next: Notif

--- main.js
+++ main.js
@@ -2,8 +2,23 @@ const timer = {
     pomodoro: 25,
     shortBreak: 5,
     longBreak: 15,
-    longBreakInterval: 4
+    longBreakInterval: 4,
+    sessions: 0,
 };
+
+let interval;
+
+const buttonSound = new Audio('button-sound.mp3');
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+    buttonSound.play();
+    const { action } = mainButton.dataset;
+    if (action === 'start') {
+        startTimer();
+    } else {
+        stopTimer();
+    }
+});
 
 const modeButtons = document.querySelector('#js-mode-buttons');
 
@@ -18,6 +33,10 @@ function handleMode(event) {
     if(!mode) return;
 
     switchMode(mode);
+    //stop timer if any button from pomo nav is clicked
+    startToggle = document.querySelector('#start-stop-icon');
+    stopToggle.classList.toggle('bx-start');
+    quitTimer();
 }
 
 function switchMode(mode) {
@@ -32,9 +51,87 @@ function switchMode(mode) {
         .querySelectorAll('button[data-mode]')
         .forEach(e => e.classList.remove('active'));
     document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
-    document.body.style.backgroundColor = `var(--${mode})`;
+    // document.body.style.backgroundColor = `var(--${mode})`;
+    document
+        .getElementById('js-progress')
+        .setAttribute('max', timer.remainingTime.total);
 
     updateClock();
+}
+
+function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+
+    return {
+        total,
+        minutes,
+        seconds,
+    };
+}
+
+function startTimer() {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000;
+
+    stopToggle = document.querySelector('#start-stop-icon');
+
+    if (timer.mode === 'pomodoro') timer.sessions++;
+
+    mainButton.dataset.action = 'stop';
+    stopToggle.classList.toggle('bx-stop');
+    mainButton.classList.add('active');
+
+    interval = setInterval(function() {
+        timer.remainingTime = getRemainingTime(endTime);
+        updateClock();
+
+        total = timer.remainingTime.total;
+
+        if (total <= 0) {
+        clearInterval(interval);
+        switch (timer.mode) {
+            case 'pomodoro':
+                if (timer.sessions % timer.longBreakInterval === 0) {
+                    switchMode('longBreak');
+                } else {
+                    switchMode('shortBreak');
+                }
+                break;
+                default:
+                    switchMode('pomodoro');
+        }
+        document.querySelector(`[data-sound="${timer.mode}"]`).play();
+        startTimer();
+        }
+
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+
+    startToggle = document.querySelector('#start-stop-icon');
+  
+    mainButton.dataset.action = 'start';
+    // mainButton.textContent = 'stop';
+    startToggle.classList.toggle('bx-stop');
+    mainButton.classList.remove('active');
+}
+
+function quitTimer() {
+    clearInterval(interval);
+
+    startToggle = document.querySelector('#start-stop-icon');
+  
+    mainButton.dataset.action = 'start';
+    // mainButton.textContent = 'stop';
+    startToggle.classList.toggle('bx-start');
+    mainButton.classList.remove('active');
 }
 
 function updateClock() {
@@ -48,4 +145,14 @@ function updateClock() {
     
     min.textContent = minutes;
     sec.textContent = seconds;
+
+    const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+    document.title = `${minutes}:${seconds} — ${text}`;
+
+    const progress = document.getElementById('js-progress');
+    progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
+});
