45ea1955d71cb81714eeb92719cf9add30d709c1
Andrea San Felipe <andrea.sanfelipe.pixel8@gmail.com>
2023-11-07 16:08:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ function stopTimer() {
   clearInterval(interval);
 
   mainButton.dataset.action = "start";
-  toggleIcon.classList.toggle("bx-play");
+  //   toggleIcon.classList.toggle("bx-play");
   mainButton.classList.remove("active");
 
   //   mainButton.dataset.action = "start";
